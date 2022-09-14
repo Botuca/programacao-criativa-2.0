4a17c989085dc9248d67f760788ba9cf5ed84706
Botuca <botucacontato@gmail.com>
2022-09-14 10:00:30 -0300

    added multiple reactive sound circles

--- sketch-audio.js
+++ sketch-audio.js
@@ -1,4 +1,5 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -10,6 +11,8 @@ let audioContext, audioData, sourceNode, analyserNode;
 let manager;
 
 const sketch = () => {
+  const bins = [4, 12, 37];
+
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
@@ -17,16 +20,27 @@ const sketch = () => {
     if (!audioContext) return;
     analyserNode.getFloatFrequencyData(audioData);
 
-    const avg = getAverage(audioData);
+    for (let i = 0; i < bins.length; i++) {
+      const bin = bins[i];
+      const mapped = math.mapRange(
+        audioData[bin],
+        analyserNode.minDecibels,
+        analyserNode.maxDecibels,
+        0,
+        1,
+        true
+      );
+      const radius = mapped * 300;
 
-    context.save();
-    context.translate(width * 0.5, height * 0.5);
-    context.lineWidth = 10;
+      context.save();
+      context.translate(width * 0.5, height * 0.5);
+      context.lineWidth = 10;
 
-    context.beginPath();
-    context.arc(0, 0, Math.abs(avg), 0, Math.PI * 2);
-    context.stroke();
-    context.restore();
+      context.beginPath();
+      context.arc(0, 0, radius, 0, Math.PI * 2);
+      context.stroke();
+      context.restore();
+    }
   };
 };
 
@@ -54,11 +68,11 @@ const createAudio = () => {
   sourceNode.connect(audioContext.destination);
 
   analyserNode = audioContext.createAnalyser();
+  analyserNode.fftSize = 512;
+  analyserNode.smoothingTimeConstant = 0.9;
   sourceNode.connect(analyserNode);
 
   audioData = new Float32Array(analyserNode.frequencyBinCount);
-
-  // console.log(audioData.length);
 };
 
 const getAverage = (data) => {
